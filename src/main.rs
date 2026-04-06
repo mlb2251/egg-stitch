83e410bbb7778aba35ae89324d196de077af2058
Maddy Bowers <mlbowers@mit.edu>
2026-04-06 14:51:35 -0400

    small reffactor

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ use search::{SharedSearchData, SearchState};
 use smc::compute_cost;
 
 fn main() {
-    let rules = "../babble/harness/data/benchmark-dsrs/drawings.dials.rewrites";
-    let (egraph, root) = util::load_egraph("data/domains/cogsci/dials.json", Some(rules));
+    let rules = Some("../babble/harness/data/benchmark-dsrs/drawings.dials.rewrites");
+    let follow = Some("(T (T (T l (M 1 0 -0.5 0)) (M #0 (/ π 4) 0 0)) (M 1 0 (* #0 (* 0.5 (cos (/ π 4)))) (* #0 (* 0.5 (sin (/ π 4)))))))");
+    let (egraph, root) = util::load_egraph("data/domains/cogsci/dials.json", rules);
 
     // smc::smc(egraph, root, None);
-    smc::smc(egraph, root, Some("(T (T (T l (M 1 0 -0.5 0)) (M #0 (/ π 4) 0 0)) (M 1 0 (* #0 (* 0.5 (cos (/ π 4)))) (* #0 (* 0.5 (sin (/ π 4)))))))"));
+    smc::smc(egraph, root, follow);
 
     // let extractor = egg::Extractor::new(&egraph, egg::AstSize);
     // let (_, term) = extractor.find_best(root);
