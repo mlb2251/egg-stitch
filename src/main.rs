306571703a1f7a4048175c4462f6da8d6b6d9d0b
Maddy Bowers <mlbowers@mit.edu>
2026-04-08 14:51:17 -0400

    \pi -> pi

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use smc::compute_cost;
 
 fn main() {
     let rules = Some("../babble/harness/data/benchmark-dsrs/drawings.dials.rewrites");
-    let follow = Some("(T (T (T l (M 1 0 -0.5 0)) (M #0 (/ π 4) 0 0)) (M 1 0 (* #0 (* 0.5 (cos (/ π 4)))) (* #0 (* 0.5 (sin (/ π 4)))))))");
+    let follow = Some("(T (T (T l (M 1 0 -0.5 0)) (M #0 (/ pi 4) 0 0)) (M 1 0 (* #0 (* 0.5 (cos (/ pi 4)))) (* #0 (* 0.5 (sin (/ pi 4)))))))");
     // let follow = None;
     let (egraph, root) = util::load_egraph("data/domains/cogsci/dials.json", rules);
 
