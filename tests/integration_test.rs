be84348bb86f6bb8370dfe22f0036a5862865f71
Kavi Gupta <kavi@globalmbox.com>
2026-06-09 12:17:05 -0700

    Revert "add option to only use DSRs at the start (#225)"
    This reverts commit 20ba68525ab9d1447000cb6faa2e10a67be2bb96.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -16,13 +16,13 @@ fn fixtures_present() -> bool {
 }
 
 fn run(args: &Args) -> smc::SmcResult<OpChildren, Op> {
-    let (data, _, _) = io::load_egraph::<OpChildren, Op>(&args.input, args.rules.as_deref(), args.only_use_dsrs_at_start, Weights::default());
+    let (data, _, _) = io::load_egraph::<OpChildren, Op>(&args.input, args.rules.as_deref(), Weights::default());
     let mut rng = StdRng::seed_from_u64(args.seed.unwrap_or(0));
     smc::smc(data, args, &mut rng)
 }
 
 fn run_lambda_calc(args: &Args) -> smc::SmcResult<LambdaCalc, OpDB<Op>> {
-    let (data, _, _) = io::load_egraph::<LambdaCalc, OpDB<Op>>(&args.input, args.rules.as_deref(), args.only_use_dsrs_at_start, Weights::default());
+    let (data, _, _) = io::load_egraph::<LambdaCalc, OpDB<Op>>(&args.input, args.rules.as_deref(), Weights::default());
     let mut rng = StdRng::seed_from_u64(args.seed.unwrap_or(0));
     smc::smc(data, args, &mut rng)
 }
