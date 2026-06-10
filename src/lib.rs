dfd984a72c21bd1153167209c872a5f9daaf9a70
Kavi Gupta <kavi@berkeley.edu>
2026-06-10 01:01:57 -0400

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -194,8 +194,8 @@ pub struct Args {
     ///
     /// We keep only patterns with ForcedExpansion(p) ≤ `max_forced_expansion`
     ///
-    /// To turn off this prune, set to `none`. Default 10.
-    #[arg(long = "max-forced-expansion", default_value = "10")]
+    /// To turn off this prune, set to `none`. Default 5.
+    #[arg(long = "max-forced-expansion", default_value = "5")]
     pub max_forced_expansion: MaxForcedExpansion,
 
     /// Path to write JSON output.
