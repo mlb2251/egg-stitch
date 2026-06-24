0b01be7f44e50cfaad364e5cd5b28a095a9cd407
Maddy Bowers <mlbowers@mit.edu>
2026-06-24 09:13:57 -0400

    subterm avoidance

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -15,7 +15,7 @@ pub use lambda_calc::{LambdaCalcDisc, LambdaCalcLanguage};
 pub use op::{Op, StitchDisc, StitchOp, de_bruijn_strictly_more_expensive_than_symbols};
 pub use op_children::OpChildrenLanguage;
 pub use op_db::OpDB;
-pub use op_with_frozen_var::OpWithFrozenVar;
+pub use op_with_frozen_var::{FROZEN_ROOT_NAME, OpWithFrozenVar};
 pub use op_with_var::OpWithVar;
 
 /// Trait covering every language usable with the search machinery.
